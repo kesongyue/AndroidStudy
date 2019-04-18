af9a464f3d13daca4d3794f5deeec111a1ad99c1
kesongyue <13018597516@163.com>
2019-04-18 10:29:28 +0800

    Update MainActivity.java

--- Lab-SimpleWebView/app/src/main/java/com/example/ksy/lab_simplewebview/MainActivity.java
+++ Lab-SimpleWebView/app/src/main/java/com/example/ksy/lab_simplewebview/MainActivity.java
@@ -26,7 +26,20 @@ public class MainActivity extends AppCompatActivity {
                 String url = urlEditText.getText().toString();
                 webView.getSettings().setJavaScriptEnabled(true);
                 //webView.setWebViewClient(new WebViewClient());
+               //Todo: 绑定固定的URL
                 webView.loadUrl("https://www.baidu.com/");
+
+//                下面是测试从H5跳转回Android原生的代码
+//                if(mui.os.android){
+//                    var main = plus.android.runtimeMainActivity();
+//                    var Intent = plus.android.importClass("android.content.Intent");
+//                    var intent = new Intent(main.getIntent());
+//                    intent.setClassName(main, "com.sysu.Smart_health.SimpleWebviewActivity");
+//                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                    main.startActivity(intent);
+//                }else {
+//                    mui.alert('目前一键开锁功能仅支持安卓平台。', '提示', null);
+//                }
             }
         });
     }
